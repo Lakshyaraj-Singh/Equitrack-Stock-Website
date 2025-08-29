1334253ea7ce1075c5aa1fab7697712d2747cc7c
Lakshyaraj Singh Ranawat <lakshyaraj.singh.05@gmail.com>
2025-08-29 20:46:19 +0530

    Sell And Buy Modals Ready With UI

--- frontend/src/Dashboard/components/UserOptionsOnDashboard/BuyBox.jsx
+++ frontend/src/Dashboard/components/UserOptionsOnDashboard/BuyBox.jsx
@@ -1,26 +1,64 @@
 import { useEffect } from "react";
 import { useRef } from "react";
-export const BuyBox = ({stock,oncloseModal}) => {
-    let modalRef=useRef();
+export const BuyBox = ({ stock, oncloseModal }) => {
+    let modalRef = useRef();
     useEffect(() => {
-      
+
         console.log(stock);
-    
-      
+
+
     }, []);
-    const close=(e)=>{
-        if(modalRef.current==e.target){
+    const close = (e) => {
+        if (modalRef.current == e.target) {
             oncloseModal();
 
         }
     }
-    
-  return (
-    <div ref={modalRef} onClick={close} className="w-screen h-screen  backdrop-blur-xs place-content-center place-items-center absolute  inset-0 z-10 rounded">
-        
-        <div  className="bg-white w-[60%] mb-20">BUYINF
-            {/* symbol */}{stock.name}
+
+    return (
+        <div ref={modalRef} onClick={close} className="w-screen h-screen  backdrop-blur-xs place-content-center place-items-center absolute  inset-0 z-10 rounded">
+
+            <div className="bg-white w-[40%] mb-20 shadow-2xl shadow-black">
+                Buying
+                <div className="flex flex-col gap-5 w-full items-center p-3">
+                    {/* stock name and symbol */}
+                    <div className="flex justify-between rounded-2xl  bg-blue-100 w-full  p-3">
+
+                        <div className=" flex gap-2 flex-col "><h1 className="font-extrabold text-2xl">{/*{stock.symbol}  */}{stock.name}</h1>  <p className="text-sm text-gray-500">Apple.Inc</p></div>
+                        <div className="flex gap-2 flex-col"> <h1 className="font-bold text-2xl">$182.33</h1> <p className="text-xs text-white
+                        bg-black rounded-3xl ">+2.34(1.28%)</p></div>
+                    </div>
+
+                    {/* Order Type */}
+                    <div className="w-full ">
+                        <h2 className="text-xs font-bold">Order Type</h2>
+                        <select className="bg-gray-100 w-full text-xs rounded-md p-2 " name="Otype" id="Otype"><option className="text-xs" value="Market Order">Market order</option></select>
+                        <p className="text-xs text-gray-800 mt-2">Buy immediately at current market price </p>
+                    </div>
+
+                  {/* quantity of stock */}
+                   <div className="w-full ">
+                    <h1 className="text-xs font-bold">Quantity</h1>
+                    <input className="bg-gray-100 w-full pl-2 rounded-md" type="number" min={1} value={1}   name="qty" id="qty" />
+                   </div>
+                  
+                  {/* Full summary per share */}
+                  <div className="bg-gray-200 rounded-lg p-3 w-full space-y-2.5">
+                    <div className="text-xs font-semibold flex justify-between"><h1 className="text-gray-500">Price per share:</h1> <p>$189.0</p></div>
+                    <div className="text-xs font-semibold  flex justify-between"><h1 className="text-gray-500">Quantity</h1> <p>1 Shares</p></div>
+                    <hr />
+                    <div className="text-sm flex justify-between"><h1 className="font-bold ">Total Cost:</h1> <p className="font-bold">$189.0</p></div>
+                       
+                  </div>
+
+                  {/* actionbuttons */}
+                  <div className="text-xs w-full flex justify-evenly gap-1">
+                    <button onClick={()=>{oncloseModal()}} className="p-2 border-[1px] border-gray-400 rounded-lg  w-full">Cancel</button>
+                    <button className="p-2  w-full bg-green-600 text-white rounded-lg ">Buy Share</button>
+                  </div>
+                </div>
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
