bbea9aab24dca8d9bbdc82b7bb977cddeb7dbb74
Lakshyaraj Singh Ranawat <lakshyaraj.singh.05@gmail.com>
2025-09-06 15:38:42 +0530

    Initializing buyStock Route

--- backend/Routes/Stock.js
+++ backend/Routes/Stock.js
@@ -5,4 +5,8 @@ router.get("/allStocks",stock.AllStocksSummary)
 router.post("/particularStockDetail",stock.particularDetailStock)
 router.post("/StockSnapshot",stock.chartMonth)
 router.post("/particularStock",stock.particularStock)
+
+
+//Stock Actions
+router.post("/buyStock",stock)
 export default router;
