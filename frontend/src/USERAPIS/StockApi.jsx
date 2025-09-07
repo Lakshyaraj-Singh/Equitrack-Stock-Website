371d6787f38a8f6c9d7134d7b4707dc003d96b1e
Lakshyaraj Singh Ranawat <lakshyaraj.singh.05@gmail.com>
2025-09-07 16:30:34 +0530

    Api call function Buyingstock

--- frontend/src/USERAPIS/StockApi.jsx
+++ frontend/src/USERAPIS/StockApi.jsx
@@ -1,42 +1,53 @@
 // STOCK API
 
-import axios from'axios';
+import axios from 'axios';
 
-const api=axios.create({
-    baseURL:"http://localhost:3000/api/stk"
+const api = axios.create({
+    baseURL: "http://localhost:3000/api/stk"
 })
 // get("/allStocks",stock.AllStocksSummary)
 // post("/paricularStock",stock.particularStock)
 // post("/StockSnapshot",stock.chartMonth)
 
-export const dashBoardAllStock=async()=>{
-    try{
-       let res=await api.get(`/allStocks`);
-       return res;
+export const dashBoardAllStock = async () => {
+    try {
+        let res = await api.get(`/allStocks`);
+        return res;
     }
-    catch(error){
+    catch (error) {
         return error.response
     }
 }
 
-export const stockDetail=async(Name)=>{
-    const Data={stockName:Name}
-    try{
-       let res=await api.post("/particularStockDetail",Data);
-       return res;
+export const stockDetail = async (Name) => {
+    const Data = { stockName: Name }
+    try {
+        let res = await api.post("/particularStockDetail", Data);
+        return res;
     }
-    catch(error){
+    catch (error) {
         return error.response;
     }
 }
 
-export const stockActions=async(Name)=>{
-    const Data={stockName:Name}
-    try{
-       let res=await api.post("/particularStock",Data);
-       return res;
+export const stockActions = async (Name) => {
+    const Data = { stockName: Name }
+    try {
+        let res = await api.post("/particularStock", Data);
+        return res;
     }
-    catch(error){
+    catch (error) {
+        return error.response;
+    }
+}
+
+export const buyingStocksAction = async (data) => {
+     let token=localStorage.getItem("token")
+    try {
+        let res = await api.post("/buyStock", data,{headers:{'Authorization':`Bearer ${token}`}});
+        return res;
+    }
+    catch (error) {
         return error.response;
     }
 }
