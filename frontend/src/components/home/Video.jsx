2f9c43b9d97fcf0fee6983ebeb841e5c37fa0cc2
Lakshyaraj Singh Ranawat <lakshyaraj.singh.05@gmail.com>
2025-09-28 11:42:57 +0530

    P2

--- frontend/src/components/home/Video.jsx
+++ frontend/src/components/home/Video.jsx
@@ -6,7 +6,7 @@ export const Video = () => {
   return (
     <div className=' relative h-[700px] md:h-fit    place-items-center ' >
      <video className='object-cover  w-full h-full md:w-fit md:h-fit'  autoPlay muted loop style={{filter:"brightness(50%)" }} >
-         <source src="src\assets\21117-315137086_small.mp4"  type="" />
+         <source src="/21117-315137086_small.mp4"  type="video/mp4" />
        </video>
        <div className='absolute top-1/2  text-white  left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
 
