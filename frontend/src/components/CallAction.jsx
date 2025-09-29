6e18766412916074b08ca31062bff448c4f97c8f
Lakshyaraj Singh Ranawat <lakshyaraj.singh.05@gmail.com>
2025-09-29 11:05:26 +0530

    Deployed

--- frontend/src/components/CallAction.jsx
+++ frontend/src/components/CallAction.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom';
 export const CallAction = () => {
+let navigate=useNavigate()
   return (
     <div className='flex flex-col  max-w-screen text-center items-center gap-7'>
     <h1 className='font-bold md:text-6xl text-3xl'>Invest in everything</h1>
     <p className='md:text-xl'>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-    <button className='bg-sky-600 hover:bg-sky-700 hover:shadow hover:shadow-blue-300 transition-all duration-150 text-white py-3 rounded font-bold text-xl w-56 '>Sign up for free</button>
+    <button onClick={()=>navigate("/signup")}  className='bg-sky-600 hover:bg-sky-700 hover:shadow hover:shadow-blue-300 transition-all duration-150 text-white py-3 rounded font-bold text-xl w-56 '>Sign up for free</button>
   </div>
   )
 }
--- frontend/src/components/products/Kite.jsx
+++ frontend/src/components/products/Kite.jsx
@@ -6,14 +6,14 @@ import { RevealFade,Fade, LeftReveal } from '../Framer'
 export const Kite = () => {
   return (
     <div className='flex flex-col gap-50  md:gap-10 mb-7 p-10'>
-      <Fade delay={0.5}>
+      <Fade delay={1} >
 
       <LeftSide imgs="./coin.png" para="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices." title="Track" anc="Try Now →"/>
       </Fade>
-   <RevealFade duration={1}>
+   <RevealFade  duration={0.5}>
    <RightSide imgs="./console.png" para="The central dashboard for your Zerodha account. Gain insights into your trades and investments with in-depth reports and visualisations." title="Console" anc="Learn more"/>
     </RevealFade> 
-   <LeftReveal duration={1} ><LeftSide imgs="./varsity.png" title="Varsity mobile" para="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go."/>
+   <LeftReveal duration={0.8} ><LeftSide imgs="./varsity.png" title="Varsity mobile" para="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go."/>
     </LeftReveal> 
     <hr />
     </div>
